5957f4985450e924d7cee7cc431b7b115f97f8a0
Julina778 <166202094+Julina778@users.noreply.github.com>
2025-05-12 23:59:34 +0800

    Update alesstya.h

--- src/game/client/components/alesstya/alesstya.h
+++ src/game/client/components/alesstya/alesstya.h
@@ -1,8 +1,8 @@
 #ifndef GAME_CLIENT_COMPONENTS_ALESSTYA_H
 #define GAME_CLIENT_COMPONENTS_ALESSTYA_H
 
-#include <game/client/component.h>
 #include <engine/console.h>
+#include <game/client/component.h>
 
 class CAlesstya : public CComponent
 {
