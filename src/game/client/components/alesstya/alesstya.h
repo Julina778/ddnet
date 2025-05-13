9211ee2c45632443d4b24b3f351f34d4a9ac80c1
778 tantyy <tanyongyih123@gmail.com>
2025-05-13 21:45:22 +0800

    AutoReply功能删减版 (没有AutoReplyMuted)

--- src/game/client/components/alesstya/alesstya.h
+++ src/game/client/components/alesstya/alesstya.h
@@ -1,15 +1,22 @@
-#ifndef GAME_CLIENT_COMPONENTS_ALESSTYA_H
-#define GAME_CLIENT_COMPONENTS_ALESSTYA_H
+#ifndef GAME_CLIENT_COMPONENTS_ALESSTYA_ALESSTYA_H
+#define GAME_CLIENT_COMPONENTS_ALESSTYA_ALESSTYA_H
 
-#include <game/client/component.h>
 #include <engine/console.h>
+#include <game/client/component.h>
 
 class CAlesstya : public CComponent
 {
+	class IEngineGraphics *m_pGraphics = nullptr;
+
+	char m_PreviousOwnMessage[2048] = {};
+
+	bool SendNonDuplicateMessage(int Team, const char *pLine);
+
 public:
 	CAlesstya();
 	int Sizeof() const override { return sizeof(*this); }
 	void OnInit() override;
+	void OnMessage(int MsgType, void *pRawMsg) override;
 };
 
 #endif
